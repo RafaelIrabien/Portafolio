a06aadec409b9cf950273f876bfea0772618b211
RafaelIrabien <rafa.irabien@gmail.com>
2023-05-29 00:39:14 -0600

    Agregar perfil de presentación

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portafolio</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -16,10 +16,39 @@
                     <ul>
                         <li><a href="">Inicio</a></li>
                         <li><a href="">Portafolio</a></li>
-                        <li><a href="">Sobre mi</a></li>
+                        <li><a href="">Sobre mí</a></li>
                     </ul>
                 </nav>
             </div>
+
+            <img src="img/fondo_header.jpg" alt="">
+        </div>
+
+
+        <div class="profile">
+            <div class="square"></div>
+            <div class="circle-trans"></div>
+            <div class="circle"></div>
+            <div class="profile-img">
+                <img src="img/Rafa.png">
+            </div>
+
+            <div class="square-text">Rafael Antonio Irabien Criollo</div>
+            <div class="square-text2"><span>ING. Desarrollo y Gestión de Software</span></div>
+            <div class="contact-icon">
+                <a href=""><img src="img/github.svg" alt=""></a>
+                <a href=""><img src="img/linkedin.svg" alt=""></a>
+                <a href=""><img src="img/message.svg" alt=""></a>
+            </div>
+
+            <div class="square-text3">
+                <a href="#about-me">
+                    <span>Sobre mí</span>
+                </a>
+            </div>
+
+                
+            
         </div>
     </header>
     

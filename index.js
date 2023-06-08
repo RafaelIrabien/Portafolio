66a8e7de03121acc41ad28f67c9a6d951685cc3c
RafaelIrabien <rafa.irabien@gmail.com>
2023-06-07 22:51:24 -0600

    Crear versión optimizada del proyecto en carpeta 'dist'

---
+++ index.js
@@ -0,0 +1,6 @@
+//Importamos todos los archivos js que estemos utilizando
+import './node_modules/eins-modal/dist/js/eins-modal.min.js';
+
+////Importamos todos los archivos css que estemos utilizando
+import './node_modules/eins-modal/dist/css/eins-modal.min.css';
+import './style.css';

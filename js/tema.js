bad3548e498465971a51c3afa729b0f78ed50530
RafaelIrabien <rafa.irabien@gmail.com>
2023-08-04 23:34:38 -0600

    Hacer responsivo el menu

--- js/tema.js
+++ js/tema.js
@@ -10,3 +10,40 @@ themeToggleBtn.onclick=()=>{
     inner.classList.toggle("active");
     isDark!=isDark;
 }
+
+
+/*
+// Esperar a que se cargue completamente el contenido del DOM
+document.addEventListener("DOMContentLoaded", function () {
+    // Obtener todos los elementos con la clase "icon_link"
+    const hoverLinks = document.querySelectorAll(".icon_link");
+  
+    // Recorrer los elementos y agregar los eventos
+    hoverLinks.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        // Mostrar el texto oculto con un efecto horizontal lento
+        const hiddenText = link.querySelector(".texto");
+        if (hiddenText) {
+          showTextSlowly(hiddenText, 0.5); // 0.5 segundos para la duración de la animación (puedes ajustarlo)
+        }
+      });
+  
+      link.addEventListener("mouseout", () => {
+        // Ocultar el texto oculto
+        const hiddenText = link.querySelector(".texto");
+        if (hiddenText) {
+          hiddenText.style.opacity = 0;
+          hiddenText.style.transform = "translateX(-100%)";
+        }
+      });
+    });
+  });
+  
+  // Función para mostrar el texto lentamente de manera horizontal
+  function showTextSlowly(element, duration) {
+    element.style.opacity = 1;
+    element.style.transform = "translateX(0)";
+  
+    element.style.transition = `opacity ${duration}s, transform ${duration}s`;
+  }
+  */
